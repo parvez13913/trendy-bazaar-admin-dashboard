127709149d0a526cdfc2d637c8d3a76ddf5e9ccd
parvez <parvezz13913@gmail.com>
2024-11-19 16:36:45 +0600

    [DONE] Reusable Form and Form input component

---
+++ src/components/Froms/Form.tsx
@@ -0,0 +1,41 @@
+import React, { ReactElement, ReactNode, useEffect } from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+
+type FormConfig = {
+  defaultValues?: Record<string, any>;
+  resolver?: any;
+};
+
+type FormProps = {
+  children?: ReactElement | ReactNode;
+  submitHandler: SubmitHandler<any>;
+} & FormConfig;
+
+const From: React.FC<FormProps> = ({
+  children,
+  submitHandler,
+  defaultValues,
+  resolver,
+}) => {
+  const fromConfig: FormConfig = {};
+  if (!!defaultValues) fromConfig["defaultValues"] = defaultValues;
+  if (!!resolver) fromConfig["resolver"] = resolver;
+
+  const method = useForm<FormProps>(fromConfig);
+  const { handleSubmit, reset } = method;
+
+  const onSubmit = (data: any) => {
+    submitHandler(data);
+    reset();
+  };
+
+  useEffect(() => reset(defaultValues), [defaultValues, reset, method]);
+
+  return (
+    <FormProvider {...method}>
+      <form onSubmit={handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default From;
---
+++ src/components/Froms/FormInput.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { GetErrorMessage } from "../../utils/GetErrorMessage";
+import { Input } from "../ui/input";
+
+interface FormInputProps {
+  name: string;
+  type?: string;
+  value?: string | string[] | undefined;
+  id?: string;
+  placeholder?: string;
+  validation?: object;
+  label?: string;
+  required?: boolean;
+}
+
+const FormInput: React.FC<FormInputProps> = ({
+  name,
+  type,
+  value,
+  id,
+  placeholder,
+  validation,
+  label,
+  required,
+}) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+  const errorMessage = GetErrorMessage(errors, name);
+  return (
+    <>
+      {required ? <span className="text-red-500">*</span> : null}
+      {label ? label : null}
+      <Controller
+        control={control}
+        name={name}
+        render={({ field }) =>
+          type === "password" ? (
+            <Input
+              type="password"
+              placeholder={placeholder}
+              {...field}
+              value={value ? value : field.value}
+            />
+          ) : (
+            <Input
+              type={type}
+              placeholder={placeholder}
+              {...field}
+              value={value ? value : field.value}
+            />
+          )
+        }
+      />
+      <small className="text-red-500">{errorMessage}</small>
+    </>
+  );
+};
+
+export default FormInput;
