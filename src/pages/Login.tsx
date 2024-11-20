52c760533de8c3ed169708ff0e4b77c5b4e8eeb5
parvez <parvezz13913@gmail.com>
2024-11-20 19:38:02 +0600

    Modified the code

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
 import { Button } from "@/components/ui/button";
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const LoginPage: React.FC = () => {
@@ -9,10 +10,15 @@ const LoginPage: React.FC = () => {
 
   return (
     <section>
-      <h2 className="text-2xl font-semibold text-center my-4 lg:my-10">
-        Trendy Bazar
-      </h2>
-      <div className="flex justify-center items-center">
+      <div className="flex items-center justify-center mt-4 lg:mt-10">
+        <h2 className="rounded-full border p-6 h-32 w-32 text-center">
+          <FaShoppingCart className="w-10 h-10 mx-auto text-[#0D7C66]" />
+          <span className="font-semibold text-xs mt-4 line-clamp-none">
+            Trendy Bazar
+          </span>
+        </h2>
+      </div>
+      <div className="flex justify-center items-center mb-4">
         <div className="w-11/12 lg:w-1/2 px-4 py-10 rounded-lg shadow-md">
           <h1 className="text-xl font-semibold">Login to account</h1>
           <p className="text-sm text-muted-foreground my-2">
@@ -43,7 +49,7 @@ const LoginPage: React.FC = () => {
                 </div>
               </div>
               <div className="space-y-2">
-                <Button variant="default" className="w-full" type="submit">
+                <Button className="w-full bg-[#0D7C66]" type="submit">
                   Login
                 </Button>
               </div>
@@ -51,7 +57,10 @@ const LoginPage: React.FC = () => {
           </Form>
           <div className="text-left mt-3 text-sm">
             {"You don't have an account yet? "}
-            <Link to="#" className="text-primary hover:underline">
+            <Link
+              to="#"
+              className="text-primary hover:underline hover:text-[#0D7C66]"
+            >
               Register Now
             </Link>
           </div>
