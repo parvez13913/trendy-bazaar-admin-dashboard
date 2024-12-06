5487e940e5e13f7ae088d47d0d695686e7014498
parvez <parvezz13913@gmail.com>
2024-12-06 22:45:41 +0600

    [DONE] Sidebar setup

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import AppSidebar from "../ui/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
+import { SidebarProvider } from "../ui/sidebar";
 
 const MainLayout = () => {
   return (
     <SidebarProvider>
       <AppSidebar />
       <main>
-        <SidebarTrigger />
         <Outlet />
       </main>
     </SidebarProvider>
--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -1,5 +1,50 @@
-import { Sidebar } from "./sidebar";
+import { FaHome } from "react-icons/fa";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarTrigger,
+} from "./sidebar";
 
-export default function AppSidebar() {
-  return <Sidebar collapsible="icon"></Sidebar>;
-}
+const AppSidebar = () => {
+  const items = [
+    {
+      title: "Home",
+      url: "#",
+      icon: FaHome,
+    },
+  ];
+
+  return (
+    <Sidebar collapsible="icon">
+      <div className="flex items-end justify-end">
+        <SidebarTrigger />
+      </div>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Trendy Bazar</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+export default AppSidebar;
