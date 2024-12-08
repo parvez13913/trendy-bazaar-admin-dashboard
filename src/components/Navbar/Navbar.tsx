12a12e07b1ce5e06927ad765939e1718995cf028
parvez <parvezz13913@gmail.com>
2024-12-08 23:50:44 +0600

    Modified the code

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,11 @@
 import { Link } from "react-router-dom";
-import MainNav from "./MainNav";
 
 const NavBar = () => {
   return (
-    <div className="border-b sticky top-0 bg-primary-foreground">
+    <div className="border-b sticky top-0">
       <div className="h-16 px-8 flex items-center">
-        <div>
-          <MainNav className="mx-8" />
-        </div>
         <div className="ml-auto flex items-center space-x-4">
-          <Link
-            className="cursor-pointer bg-primary text-white p-3 rounded-sm font-normal hover:opacity-50"
-            to="/login"
-          >
+          <Link className="cursor-pointer hover:opacity-50" to="/login">
             Login
           </Link>
         </div>
