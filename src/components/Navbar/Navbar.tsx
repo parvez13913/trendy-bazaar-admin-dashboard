3c1cad05b9b9b270dfe87b4d97c63ed529c68ba9
parvez <parvezz13913@gmail.com>
2024-12-09 18:30:33 +0600

    Modified the code

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,21 @@
+import { isLoggedIn } from "@/auth-service/auth-service";
+import { FaUserAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const userLoggedIn = isLoggedIn();
+
   return (
     <div className="border-b sticky top-0">
       <div className="h-16 px-8 flex items-center">
         <div className="ml-auto flex items-center space-x-4">
-          <Link className="cursor-pointer hover:opacity-50" to="/login">
-            Login
-          </Link>
+          {userLoggedIn ? (
+            <FaUserAlt />
+          ) : (
+            <Link className="cursor-pointer hover:opacity-50" to="/login">
+              Login
+            </Link>
+          )}
         </div>
       </div>
     </div>
