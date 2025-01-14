8ef344c74c3f5cfdb7b0eace2772a3f3f98f04d8
parvez <parvezz13913@gmail.com>
2025-01-14 22:35:16 +0600

    [DONE] Forgot password page design

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,9 +1,32 @@
-const ForgotPasswordPage = () => {
+import Button from "@/components/Forms/Button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+export default function ForgotPasswordPage() {
   return (
-    <div className="text-black">
-      <h1>Forgot password</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <Card className="w-full max-w-md">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-center">
+            Forgot Password
+          </CardTitle>
+          <CardDescription className="text-center">
+            Enter your email address and we'll send you a link to reset your
+            password.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Input placeholder="Email" className="my-2" />
+          <Input placeholder="Password" className="my-2" />
+          <Button className="bg-primary w-20 py-1">Send</Button>
+        </CardContent>
+      </Card>
     </div>
   );
-};
-
-export default ForgotPasswordPage;
+}
