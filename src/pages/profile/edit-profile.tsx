b864f1086c673480cd4fb961c2ebc50911f1a808
parvez <parvezz13913@gmail.com>
2025-01-21 20:18:28 +0600

    [DONE] Edit profile page design

--- src/pages/profile/edit-profile.tsx
+++ src/pages/profile/edit-profile.tsx
@@ -1,7 +1,104 @@
+"use client";
+
+import Button from "@/components/Forms/Button";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { updateProfileSchema } from "@/utils/zood-schemas/auth.validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import type { z } from "zod";
+
 const EditProfilePage = () => {
+  const [date, setDate] = useState<Date>();
+  const form = useForm<z.infer<typeof updateProfileSchema>>({
+    resolver: zodResolver(updateProfileSchema),
+  });
+
+  const onSubmit = async (data: Record<string, unknown>) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1 className="text-black">Edit Profile</h1>
+    <div className="container mx-auto px-4 py-8">
+      <Card className="w-full max-w-4xl mx-auto">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl sm:text-3xl md:text-4xl">
+            Edit Profile
+          </CardTitle>
+          <CardDescription className="text-sm sm:text-base">
+            Update your personal information here.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form submitHandler={onSubmit} {...form} className="space-y-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
+              <div>
+                <FormInput
+                  name="firstName"
+                  type="text"
+                  label="First Name"
+                  required
+                  placeholder="First Name"
+                />
+              </div>
+              <div>
+                <FormInput
+                  name="middleName"
+                  type="text"
+                  label="Middle Name"
+                  placeholder="Middle Name"
+                />
+              </div>
+
+              <div>
+                <FormInput
+                  name="lastName"
+                  type="text"
+                  label="Last Name"
+                  required
+                  placeholder="Last Name"
+                />
+              </div>
+
+              <div>
+                <FormInput
+                  name="email"
+                  type="email"
+                  label="Email"
+                  required
+                  placeholder="Email"
+                />
+              </div>
+
+              <div>
+                <FormInput
+                  name="contactNo"
+                  type="tel"
+                  label="Contact No"
+                  required
+                  placeholder="Contact No"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-center mt-8">
+              <Button
+                type="submit"
+                className="w-full sm:w-2/3 md:w-1/2 bg-gradient-to-r from-primary to-secondary hover:from-primary-foreground hover:to-secondary-foreground text-foreground font-semibold py-3 px-6 rounded-md transition duration-300 ease-in-out transform hover:scale-105"
+              >
+                Update Profile
+              </Button>
+            </div>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- src/utils/zood-schemas/auth.validation.ts
+++ src/utils/zood-schemas/auth.validation.ts
@@ -12,6 +12,55 @@ export const loginSchema = z.object({
     .max(30),
 });
 
+export const updateProfileSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, {
+      message: "First name must be at least 2 characters.",
+    })
+    .optional(),
+  middleName: z.string().optional(),
+  lastName: z
+    .string()
+    .min(2, {
+      message: "Last name must be at least 2 characters.",
+    })
+    .optional(),
+  email: z
+    .string()
+    .email({
+      message: "Please enter a valid email address.",
+    })
+    .optional(),
+  contactNo: z
+    .string()
+    .min(10, {
+      message: "Please enter a valid contact number.",
+    })
+    .optional(),
+  gender: z.enum(["Male", "Female", "Other"]).optional(),
+  bloodGroup: z
+    .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+    .optional(),
+  dateBirth: z
+    .date({
+      required_error: "Please select a date",
+    })
+    .optional(),
+  address: z
+    .string()
+    .min(5, {
+      message: "Address must be at least 5 characters.",
+    })
+    .optional(),
+  profileImage: z
+    .string()
+    .min(5, {
+      message: "Address must be at least 5 characters.",
+    })
+    .optional(),
+});
+
 export const resetPasswordSchema = z.object({
   email: z.string().email().min(2, {
     message: "Email is required",
